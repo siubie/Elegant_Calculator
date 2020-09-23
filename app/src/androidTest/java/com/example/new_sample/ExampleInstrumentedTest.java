0da1517deff2dd20f9ba01113fc9080ed87745ab
Putra Prima Arhandi <putraprima@gmail.com>
2020-09-23 08:28:20 +0700

    contoh test case

--- app/src/androidTest/java/com/example/new_sample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/new_sample/ExampleInstrumentedTest.java
@@ -8,6 +8,13 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
---
+++ app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
@@ -0,0 +1,30 @@
+package com.example.new_sample;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TesKlikButton {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void tesKlikButtonSatu(){
+        //cari id button1 terus di klik
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("1")));
+    }
+
+}
